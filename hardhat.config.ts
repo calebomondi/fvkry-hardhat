072aac570065779e0f86f91a9f4c32667338ed5a
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-02-04 00:08:07 +0300

    Update Solidity version to 0.8.28, configure Lisk Sepolia network in Hardhat, and add deployment script for Fvkry contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+require("dotenv").config();
 import 'solidity-coverage';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    // for test
+    "lisk-sepolia": {
+      url: 'https://rpc.sepolia-api.lisk.com',
+      accounts: [process.env.WALLET_KEY as string],
+      gasPrice: 1000000000,
+    }
+  }
 };
 
 export default config;
